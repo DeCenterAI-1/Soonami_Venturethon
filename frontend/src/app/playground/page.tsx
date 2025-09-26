950dea58e61b0775495053021918cc0734d413f6
Kosawat Sukchaya <kosawat@gmail.com>
2025-09-26 11:57:50 +0200

    Implemented simple playground page

---
+++ frontend/src/app/playground/page.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Playground from "../components/Playground";
+import Sidebar from "../components/Sidebar";
+
+export default function PlaygroundPage() {
+  return (
+    <div className="flex min-h-screen bg-[#050505]">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <Playground />
+      </div>
+    </div>
+  );
+}
